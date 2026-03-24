ceccbf77255fa5905c10b9bff5907501726f1319
zohrakhanG <zohrakha20@gmail.com>
2026-03-24 17:13:08 +0500

    Update urls.py

--- NatureLensProject/accounts/urls.py
+++ NatureLensProject/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, login, get_discover_plants,get_single_plant, current_user, herbal_vault_list,herbal_vault_detail,daily_featured_herb
+from .views import signup, login, get_discover_plants,get_single_plant, current_user, herbal_vault_list,herbal_vault_detail,daily_featured_herb,add_plant_journal,get_user_journals,delete_account,update_user, predict
 
 urlpatterns = [
     path('signup/', signup),
@@ -9,7 +9,11 @@ urlpatterns = [
     path('herbalVault/', herbal_vault_list),
     path('herbalVault/<int:herb_id>/', herbal_vault_detail),
     path('daily-featured-herb/', daily_featured_herb, name='featured_herb'),
-
-
     path('user/', current_user, name='current_user'),
+    path("journal/add/", add_plant_journal),
+    path("journal/", get_user_journals),
+    path("delete-account/", delete_account),
+    path('user/update/', update_user),
+    path('predict/', predict),
+
 ]
